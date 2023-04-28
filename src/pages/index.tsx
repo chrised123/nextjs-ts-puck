b6e69a48dcada84edf87ef62c828ad0b6965ab08
Sanjith Edward <sanjithchendrika@gmail.com>
2023-04-28 13:50:01 +1000

    added vercel analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="text-center w-full text-2xl mt-10">Test</div>
+    <>
+      <div className="text-center w-full text-2xl mt-10">Test</div>
+      <Analytics />
+    </>
   )
 }
